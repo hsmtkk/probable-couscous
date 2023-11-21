0cd38b2ef6275d10dd03a7ffd6b762b91f9a65a5
hsmtkk <hsmtkk@gmail.com>
2023-11-22 08:09:34 +0900

    add comprehend permission

--- iam.tf
+++ iam.tf
@@ -18,5 +18,5 @@ resource "aws_iam_role" "transcribe" {
 resource "aws_iam_role" "comprehend" {
   name                = "${var.unique_prefix}-comprehend"
   assume_role_policy  = data.aws_iam_policy_document.lambda.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole", "arn:aws:iam::aws:policy/AmazonS3FullAccess"]
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/ComprehendReadOnly"]
 }
